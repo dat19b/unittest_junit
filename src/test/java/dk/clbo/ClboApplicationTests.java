3f4adbbf7dc2a6d2c77cfd9002e6c5e4a4be7ac2
clbokea <clbo@kea.dk>
2020-09-17 09:03:41 +0200

    init

---
+++ src/test/java/dk/clbo/ClboApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.clbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
